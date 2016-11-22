bbf1082ff18452b996082a551c32c88e319829e3
bleamer <mail.bhoot@gmail.com>
2016-11-22 15:49:53 +0530

    Update NextWord.cpp

--- cpp/NextWord.cpp
+++ cpp/NextWord.cpp
@@ -10,13 +10,14 @@
 #include <algorithm>    // std::sort
 using namespace std;
 
-string& NextWord(string &s){
+string NextWord(string &s){
 	string rSorted = s;
 	string retStr = string();
 	cout << "NextWord : " << s << endl;
 	sort(rSorted.begin(), rSorted.end(),greater<char>()); // Reverse sort
 	if (rSorted == s){
 		cout << "Already reverse sorted" << endl;
+		retStr = rSorted;
 //		Return empty string
 	}
 	else{
@@ -26,8 +27,19 @@ string& NextWord(string &s){
 			// Passed string is same as returned string
 			// No reversal happened
 			rSorted = retStr;
-			sort(rSorted.begin(), rSorted.end(),greater<char>()); // Reverse sort
-			
+			sort(rSorted.begin(), rSorted.end()); // sort
+			cout << "rsorted == " << rSorted << endl;
+			for (int i = 0; i < rSorted.length(); i++){
+				if (s.at(0) < rSorted.at(i)){
+					char temp = rSorted[i];
+					rSorted[i] = s[0];
+					s[0] = temp;
+					sort(s.begin(), s.end()); // sort
+					cout << "dadadada -- "<< s.at(0) << ", "<< rSorted.at(i) << endl;
+					retStr = s;
+					break;
+				}
+			}
 		}
 	}
 
@@ -37,22 +49,28 @@ string& NextWord(string &s){
 
 int main() {
 
-	string inWord, outWord;
+	string inWord, outWord, processWord;
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "Please enter a string : ";
 	cin >> inWord;
-
+	processWord  = inWord;
 //	cout << endl << inWord << endl;
 
 	try {
-		outWord = NextWord(inWord);
+		outWord = NextWord(processWord);
 //		cout << endl << "The next word is : " << outWord;
 	}
 	catch (const std::exception &exc)
 	{
 	    // catch anything thrown within try block that derives from std::exception
 	    std::cout << endl << exc.what();
-		cout << endl << "The given word is the last word in the dictionary";
+    	cout << endl << "The given word is the last word in the dictionary";
+		return 0;
 	}
+    if (outWord == inWord){
+    	cout << endl << "The given word is the last word in the dictionary";
+    }else{
+    	cout << "The next word is: " << outWord << endl;
+    }
 	return 0;
 }
